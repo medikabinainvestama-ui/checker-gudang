404806eb986ac0f251bf3f1bb068ab4a9a7f882e
medikabinainvestama-ui <medikabinainvestama@mhealth.tech>
2026-02-25 11:57:42 +0700

    Create users.py

---
+++ users.py
@@ -0,0 +1,22 @@
+# Daftar akun tim QC
+# Format: "username": "password"
+USER_DB = {
+    "budi": "qc123",
+    "aswin": "qc123",
+    "andre": "qc123"
+    "indra": "qc123"
+    "aqsal": "qc123"
+    "yonno": "qc123"
+    "ridwan": "qc123"
+    "tri": "qc123"
+    "faris": "qc123"
+    "agus": "qc123"
+    "ilmi": "qc123"
+    "fathur": "qc123"
+    "rizki": "qc123"
+    "idal": "qc123"
+    "wibi": "qc123"
+    "fahri": "qc123"
+    "himawan": "qc123"
+    "ogik": "qc123"
+}
